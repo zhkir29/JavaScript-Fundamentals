4633c2a9f148fbce865c97735d83617d6c12f255
zhkir29 <zh2018131041@gmail.com>
2021-12-21 13:17:15 +0600

    a

---
+++ C-3-Types values and variables/null and undefined.js
@@ -0,0 +1,3 @@
+console.log(typeof(undefined))
+console.log(typeof(null))//returns "object"(no object = no value) type that means it's special object.
+// null and undefined both indicate an absense of value and can often be used interchangebly
