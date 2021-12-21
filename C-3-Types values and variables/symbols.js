574bbf3d382e655a86acc465a9942929ea7149b1
zhkir29 <zh2018131041@gmail.com>
2021-12-21 20:43:42 +0600

    some concept cleared to symosl file

--- C-3-Types values and variables/symbols.js
+++ C-3-Types values and variables/symbols.js
@@ -1,1 +1,8 @@
-console.log(toString(a))
+let strname='string name'
+let symname=Symbol('propname')
+console.log(typeof(strname))
+console.log(typeof(symname))
+let o = {}
+o[strname]=1;
+o[symname]=2;
+console.log(o[strname],o[symname])
